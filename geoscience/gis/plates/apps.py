59cfb4d3cbb9b8c23a929f12c672dd367c79f95a
Sam <samuel.scott.jennings@gmail.com>
2024-08-07 11:11:47 +0200

    rename package

--- geoscience/gis/plates/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class TectonicPlateConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "geoscience.gis.plates"
-    verbose_name = _("Tectonic Plate")
-    verbose_name_plural = _("Tectonic Plates")
--- geoscience/gis/provinces/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class ProvinceConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "geoscience.gis.provinces"
-    verbose_name = _("Geologic Province")
-    verbose_name_plural = _("Geologic Provinces")
