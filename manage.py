70d42b93ba13ac75b9790a87c45f0d19e1078c14
Sam <samuel.scott.jennings@gmail.com>
2024-09-11 12:14:31 +0200

    update deps

--- manage.py
+++ manage.py
@@ -2,17 +2,17 @@
 
 import os
 import sys
-from pathlib import Path
 
 if __name__ == "__main__":
+    sys.path.append("tests")
+    os.environ.setdefault("DJANGO_ENV", "development")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
-    os.environ.setdefault("DATABASE_URL", "postgresql://username:password@hostname:5555/database_name")
 
     from django.core.management import execute_from_command_line
 
     # This allows easy placement of apps within the interior
     # project directory.
-    current_path = Path(__file__).parent.resolve()
-    sys.path.append(str(current_path / "tests"))
+    # current_path = Path(__file__).parent.resolve()
+    # sys.path.append(str(current_path / "tests"))
 
     execute_from_command_line(sys.argv)
