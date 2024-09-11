f4f23a2e24e590daf4972d4e11916d844eb8986c
Sam <samuel.scott.jennings@gmail.com>
2024-09-11 12:13:02 +0200

    update vocabularies

--- earth_science/vocabularies/cgi/geosciml.py
+++ earth_science/vocabularies/cgi/geosciml.py
@@ -1,198 +1,238 @@
-"""Collection of vocabularies from the CGI GeoSciML RemoteVocabulary.
+"""Collection of vocabularies from the CGI GeoSciML RemoteTTLVocabulary.
 
 <https://cgi.vocabs.ga.gov.au/vocab/>
 
 note::
 
-It's currently not possible to download full vocabularies direct from the CGI website or from Research Vocabs Australia.
+It doesn't seem possible to download full vocabularies direct from the CGI website or from Research Vocabs Australia.
 Therefore, the vocabularies are linked direct to the .ttl representations stored in the official CGI-IUGS GitHub repository.
 
-https://github.com/CGI-IUGS/cgi-vocabs
+https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs
 
 """
 
 from research_vocabs.vocabularies import RemoteVocabulary
 
 
-class AlterationType(RemoteVocabulary):
+class RemoteTTLVocabulary(RemoteVocabulary):
+    def _source(self):
+        return {
+            "source": self._meta.source,
+            "format": "turtle",
+        }
+
+
+class AlterationType(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/alterationtype.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/alterationtype.ttl"
 
 
-class BoreholeDrillingMethod(RemoteVocabulary):
+class BoreholeDrillingMethod(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/boreholedrillingmethod.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/boreholedrillingmethod.ttl"
 
 
-class CompositionCategory(RemoteVocabulary):
+class CompositionCategory(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/compositioncategory.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/compositioncategory.ttl"
+        )
 
 
-class CompoundMaterialConstituentPart(RemoteVocabulary):
+class CompoundMaterialConstituentPart(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/compoundmaterialconstituentpartrole.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/compoundmaterialconstituentpartrole.ttl"
 
 
-class ConsolidationDegree(RemoteVocabulary):
+class ConsolidationDegree(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/consolidationdegree.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/consolidationdegree.ttl"
+        )
 
 
-class ContactType(RemoteVocabulary):
+class ContactType(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/contacttype.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/contacttype.ttl"
 
 
-class ConventionCodeForStrikeAndDipMeasurements(RemoteVocabulary):
+class ConventionCodeForStrikeAndDipMeasurements(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/conventioncode.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/conventioncode.ttl"
 
 
-class DeformationStyle(RemoteVocabulary):
+class DeformationStyle(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/deformationstyle.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/deformationstyle.ttl"
+        )
 
 
-class DescriptionPurpose(RemoteVocabulary):
+class DescriptionPurpose(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/descriptionpurpose.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/descriptionpurpose.ttl"
+        )
 
 
-class DeterminationMethodOrientation(RemoteVocabulary):
+class DeterminationMethodOrientation(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/determinationmethodorientation.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/determinationmethodorientation.ttl"
 
 
-class EventEnvironment(RemoteVocabulary):
+class EventEnvironment(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/eventenvironment.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/eventenvironment.ttl"
+        )
 
 
-class EventProcess(RemoteVocabulary):
+class EventProcess(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/eventprocess.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/eventprocess.ttl"
 
 
-class FaultMovementSense(RemoteVocabulary):
+class FaultMovementSense(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/faultmovementsense.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/faultmovementsense.ttl"
+        )
 
 
-class FaultMovementType(RemoteVocabulary):
+class FaultMovementType(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/faultmovementtype.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/faultmovementtype.ttl"
+        )
 
 
-class FaultType(RemoteVocabulary):
+class FaultType(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/faulttype.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/faulttype.ttl"
 
 
-class FoliationType(RemoteVocabulary):
+class FoliationType(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/foliationtype.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/foliationtype.ttl"
 
 
-class GeneticCategory(RemoteVocabulary):
+class GeneticCategory(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/geneticcategory.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/geneticcategory.ttl"
+        )
 
 
-class GeologicUnitMorphology(RemoteVocabulary):
+class GeologicUnitMorphology(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/geologicunitmorphology.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/geologicunitmorphology.ttl"
 
 
-class GeologicUnitPartRole(RemoteVocabulary):
+class GeologicUnitPartRole(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/geologicunitpartrole.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/geologicunitpartrole.ttl"
 
 
-class GeologicUnitType(RemoteVocabulary):
+class GeologicUnitType(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/geologicunittype.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/geologicunittype.ttl"
+        )
 
 
-class LineationType(RemoteVocabulary):
+class LineationType(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/lineationtype.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/lineationtype.ttl"
 
 
-class MappingFrame(RemoteVocabulary):
+class MappingFrame(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/mappedfeatureobservationmethod.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/mappedfeatureobservationmethod.ttl"
 
 
-class MetamorphicFacies(RemoteVocabulary):
+class MetamorphicFacies(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/metamorphicfacies.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/metamorphicfacies.ttl"
+        )
 
 
-class MetamorphicGrade(RemoteVocabulary):
+class MetamorphicGrade(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/metamorphicgrade.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/metamorphicgrade.ttl"
+        )
 
 
-class ObservationMethodGeologicFeature(RemoteVocabulary):
+class ObservationMethodGeologicFeature(RemoteTTLVocabulary):
     class Meta:
         source = "http://resource.geosciml.org/classifierscheme/cgi/2016.01/featureobservationmethod"
 
 
-class ObservationMethodMappedFeature(RemoteVocabulary):
+class ObservationMethodMappedFeature(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/mappedfeatureobservationmethod.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/mappedfeatureobservationmethod.ttl"
 
 
-class OrientationDeterminationMethod(RemoteVocabulary):
+class OrientationDeterminationMethod(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/determinationmethodorientation.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/determinationmethodorientation.ttl"
 
 
-class ParticleAspectRatio(RemoteVocabulary):
+class ParticleAspectRatio(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/particleaspectratio.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/particleaspectratio.ttl"
+        )
 
 
-class ParticleShape(RemoteVocabulary):
+class ParticleShape(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/particleshape.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/particleshape.ttl"
 
 
-class ParticleType(RemoteVocabulary):
+class ParticleType(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/particletype.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/particletype.ttl"
 
 
-class PlanarPolarityCode(RemoteVocabulary):
+class PlanarPolarityCode(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/planarpolaritycode.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/planarpolaritycode.ttl"
+        )
 
 
-class ProportionTerm(RemoteVocabulary):
+class ProportionTerm(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/proportionterm.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/proportionterm.ttl"
 
 
-class SimpleLithology(RemoteVocabulary):
+class SimpleLithology(RemoteTTLVocabulary):
     # This vocabulary is available from the Aus Research Vocabs website, however, we are using the CGI-IUGS GitHub repository
     # for consistency with the other vocabs. (and assuming the repository is the most up-to-date source for the vocabularies?)
 
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/simplelithology.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/simplelithology.ttl"
+        )
         # source = "https://vocabs.ardc.edu.au/registry/api/resource/downloads/214/ga_simple-lithology_v0-1.ttl"
 
 
-class StratigraphicRank(RemoteVocabulary):
+class StratigraphicRank(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/stratigraphicrank.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/stratigraphicrank.ttl"
+        )
 
 
-class ValueQualifier(RemoteVocabulary):
+class ValueQualifier(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/valuequalifier.ttl"
+        source = "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/valuequalifier.ttl"
 
 
-class RemoteVocabularyRelation(RemoteVocabulary):
+class RemoteTTLVocabularyRelation(RemoteTTLVocabulary):
     class Meta:
-        source = "https://github.com/CGI-IUGS/cgi-vocabs/blob/master/vocabularies/geosciml/vocabularyrelation.ttl"
+        source = (
+            "https://raw.githubusercontent.com/CGI-IUGS/cgi-vocabs/master/vocabularies/geosciml/vocabularyrelation.ttl"
+        )
--- earth_science/vocabularies/stratigraphy.py
+++ earth_science/vocabularies/stratigraphy.py
@@ -1,8 +1,25 @@
 from research_vocabs.vocabularies import RemoteVocabulary
 
 
-class ISC2020(RemoteVocabulary):
+class GeologicalTimescale(RemoteVocabulary):
     class Meta:
         source = "https://vocabs.ardc.edu.au/registry/api/resource/downloads/1211/isc2020.ttl"
         prefix = "isc"
         namespace = "http://resource.geosciml.org/classifier/ics/ischart/"
+        rdf_type = "gts:GeochronologicEra"
+
+
+class GeochronologicBoundary(RemoteVocabulary):
+    class Meta:
+        source = "https://vocabs.ardc.edu.au/registry/api/resource/downloads/1211/isc2020.ttl"
+        prefix = "isc"
+        namespace = "http://resource.geosciml.org/classifier/ics/ischart/"
+        rdf_type = "gts:GeochronologicBoundary"
+
+
+class EraBoundary(RemoteVocabulary):
+    class Meta:
+        source = "https://vocabs.ardc.edu.au/registry/api/resource/downloads/1211/isc2020.ttl"
+        prefix = "isc"
+        namespace = "http://resource.geosciml.org/classifier/ics/ischart/"
+        rdf_type = "gts:GeochronologicEraBoundary"
